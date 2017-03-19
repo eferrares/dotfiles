15f0d7f1cc728232babb7f2e18ef6f9725676429
Marcos Costa Pinto <costa.marcos.pro@gmail.com>
2017-03-19 10:05:25 -0300

    Removidos itens nao utilizados

--- install.sh
+++ install.sh
@@ -24,3 +24,4 @@ else
         install 
     fi
 fi
+
--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,48 +1,65 @@
 #!/bin/bash
-function exiting(){
+function exiting() {
     echo "Finalizing program..."
     echo $1
     exit
 }
 
+function apt_install() {
+    apt-get install -qy $1 && echo "=== $1 installed ==="
+}
+
+function install_docker() {
+    apt-get install -qy curl linux-image-extra-$(uname -r) linux-image-extra-virtual
+    apt-get install -qy apt-transport-https software-properties-common ca-certificates
+    curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+    apt-get install -qy software-properties-common
+    add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+    apt-get update
+    apt-get -qy install docker-engine
+
+    curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    echo "=== docker & docker-compose installed ==="
+}
+
+function install_pip() {
+    curl -XGET https://bootstrap.pypa.io/get-pip.py | python -
+    echo "=== pip installed ==="
+}
+
+function install_virtualenvwrapper() {
+    pip install virtualenvwrapper
+}
+
 [ "$USER" != root ] && exiting "You are not root!"
 
-apt-get install aplay;
-apt-get install python-software-properties;
-add-apt-repository ppa:chris-lea/node.js; apt-get update;
-add-apt-repository ppa:webupd8team/sublime-text-3; apt-get update;
-apt-get remove unity-lens-shopping;
-gsettings set com.canonical.desktop.interface scrollbar-mode normal;
+apt-get update
 
-apt-get install -y git
-echo '==='
-apt-get install -y python-dev
-echo '==='
-apt-get install -y python-pip
-echo '==='
-apt-get install -y terminator
-echo '==='
-apt-get install -y sublime-text-installer
-echo '==='
-apt-get install -y python-pygments
-echo '==='
-apt-get install -y bpython
-echo '==='
-apt-get install -y nodejs
-echo '==='
-apt-get install -y npm
+apt_install python-software-properties;
 
-echo '==='
-apt-get install -y postgresql pgadmin3
-su postgres -c psql postgres
-apt-get install postgresql-server-dev-9.5
-echo '==='
+apt_install git
+
+apt_install meld
+
+apt_install tree
+
+apt_install htop
+
+apt_install nload
+
+apt_install postgresql
+
+apt_install pgadmin3
+
+install_docker
+
+install_pip
+
 if which pip > /dev/null; then
     pip install ipython
-    echo '==='
-    pip install pep8
-    echo '==='
-    pip install virtualenvwrapper
-    echo '==='
-    pip install youtube-dl
+    echo '=== ipython installed ===='
+    install_virtualenvwrapper
+    echo '=== virtualenvwrapper installed ==='
 fi
+
